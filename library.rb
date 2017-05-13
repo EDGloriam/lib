6a5616ba1847c1f4d8105d01e644f4ec7b661fd7
edgloriam <edgloriamm@gmail.com>
2017-05-13 22:26:21 +0300

    code improved

--- library.rb
+++ library.rb
@@ -1,78 +1,47 @@
-require 'faker'
-require './suport_classes'
+require './generator'
 class Library
-	attr_accessor :books,	:orders, :readers,	:authors
+  include DataForTesting
+  attr_accessor :books, :orders, :readers, :authors
 
-	def initialize(books=[], orders=[], readers=[], authors=[])
-		@books = books
-		@orders = orders
-		@readers = readers
-		@authors = authors    # VS @authors, @books, @readers, @orders = authors, books, readers, orders
-	end
+  def initialize(books = [], orders = [], readers = [], authors = [])
+    @books = books
+    @orders = orders
+    @readers = readers
+    @authors = authors
+  end
 
-	def data(operation, file_name = "data.yaml")
-		case operation
-			when :write
-				File.open(file_name, 'w') { |file| file.write(self.to_yaml) }
-				puts "file was written. success\n\n"
-			when :read
-				 lib = YAML.load(File.open(file_name))
-					@books = lib.books
-					@orders = lib.orders
-					@readers = lib.readers
-					@authors = lib.authors
-					puts "file was loaded. success\n\n"
-			else
-				"eror. maybe you didn't pass operation(:read / :write)"
-		end
-	end
+  def data_write(file_name = 'data/data.yaml')
+    File.open(file_name, 'w') { |file| file.write(self.to_yaml) }
+    print "file was written. success\n"
+  end
 
-	def how_many
-		books = find_by_options(:book, 3)
-			p @orders.select{|order| books.reject{|item| !item.instance_of? Book}.
-					collect{|obj| obj.title}.include?(order.book.title)}.length
-	end
+  def data_load(file_name = './data/data.yaml')
+    lib = YAML.load(File.open(file_name))
+    @books = lib.books
+    @orders = lib.orders
+    @readers = lib.readers
+    @authors = lib.authors
+    print "file was loaded. success\n"
+  end
 
-	def book_lover
-		reader = find_by_options(:reader, 1)
-		  puts "#{reader.first.name} (has read books: #{reader.last})\n\n"
-	end
+  def how_many
+    books = find_best(:book, 3)
+    p @orders.select { |order| order.reader if books.include? order.book }
+      .uniq.compact.count
+  end
 
-	def favorite_book
-		book = find_by_options(:book, 1)
-			puts "#{book.first.title} (was taken #{book.last} times) \n\n"
-	end
+  def best_reader
+    puts find_best(:reader)
+  end
 
-	def generate_data
-    20.times { @authors << Author.new(Faker::Book.author,"biography") }
- 		50.times {@readers << Reader.new({name: Faker::Name.name,
-                                    			 email: Faker::Internet.email,
-                                    			 city: Faker::Address.city,
-		                                       street: Faker::Address.street_name,
-		                                       house: Faker::Address.building_number})}
- 		30.times { @books << Book.new(Faker::Book.title, @authors.sample) }
- 		100.times { @orders << Order.new(@books.sample, @readers.sample) }
-	end
+  def favorite_book
+    puts find_best(:book)
+  end
 
-		private
+  private
 
-			def find_by_options(object, desired_quantity = 1)
-				temp_hash = Hash.new(0)
-				results =[]
-
-				@orders.each do | order |
-						temp_hash[order.send(object)] += 1
-				end
-				searching_res_sorted = temp_hash.sort_by {|key, value| value}.reverse
-				searching_res_sorted.each do | item |
-							results << Array.[](item.first, item.last)
-							break if results.length === desired_quantity
-				end
-				results.flatten
-			end
-
+  def find_best(obj, quantity = 1)
+    arr = @orders.group_by(&obj).max_by(quantity) { |_, value| value.count }
+    quantity == 1 ? arr.flatten.first : arr.flatten
+  end
 end
-
-
-
-
--- run_the_program.rb
+++ run_the_program.rb
@@ -1,22 +1,17 @@
-require 'faker'
-require './suport_classes'
 require './library'
 
-puts "Welcom to Library"
-puts "================="
-
-lib=Library.new
+lib = Library.new
 lib.generate_data
-puts "Reader, who often takes a book:"
-lib.book_lover
-puts "The most popular book is:"
+
+puts 'The most popular book is:'
 lib.favorite_book
-print "Count of people, who read most popular books: "
+
+puts 'Reader, who often takes a book:'
+lib.best_reader
+
+print 'Count of people, who read most popular books:'
 lib.how_many
-puts "================================================"
+puts '-----------------------------------------------------------'
 
-lib.data :write
-lib.data :read
-
-
-
+lib.data_write
+lib.data_load
